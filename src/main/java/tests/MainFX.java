9365780afc0f7cad0e76f460a78a1efc5e952c15
Islem <islemraissi789@gmail.com>
2025-02-19 09:01:18 +0100

    Home Interface that leads to Ajouter/Afficher interface OK

--- src/main/java/tests/MainFX.java
+++ src/main/java/tests/MainFX.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainFX extends Application {
+    private static BorderPane contentArea;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,12 +18,26 @@ public class MainFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AfficherReclamations.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/HomeReclamation.fxml"));
         Parent root = loader.load();
+
+        // Store reference to content area
+        contentArea = (BorderPane) ((BorderPane) root).lookup("#contentArea");
+
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Ajouter Reclamation");
+        primaryStage.setTitle("Gestion des Réclamations");
         primaryStage.show();
+    }
 
+    // Centralized method to load FXML into content area
+    public static void loadFXML(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(MainFX.class.getResource("/" + fxmlFile));
+            Parent newContent = loader.load();
+            contentArea.setCenter(newContent);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 }
